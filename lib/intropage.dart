4331675e7e73b3febd46db57adeb759c993db67b
Makanakyjr <olayinkakenahmed@gmail.com>
2023-08-17 11:42:16 +0100

    i just added SingleChildScrollView

--- lib/intropage.dart
+++ lib/intropage.dart
@@ -16,162 +16,164 @@ class _IntroPageState extends State<IntroPage> {
   
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-          child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 30.0),
-        child: Column(
-          children: [
-            const Text(
-              "Create your account",
-              style: TextStyle(
-                fontSize: 24.0,
-                fontWeight: FontWeight.w700,
-              ),
-            ),
-            Divider(height: 30.0, color: Colors.grey),
-            const SizedBox(height: 20),
-            TextField(
-              controller: _nameController,
-              decoration: InputDecoration(
-                labelText: "Full legal first and middle name(s)",
-                hintText: "Full legal first and middle name(s)",
-                border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10.0)),
-              ),
-            ),
-            const SizedBox(height: 25.0),
-            TextField(
-              controller: _legalSurname,
-              obscureText: false,
-              decoration: InputDecoration(
-                labelText: "Full legal Surname",
-                hintText: "Full legal Surname",
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10.0),
+    return SingleChildScrollView(
+      child: Scaffold(
+        body: SafeArea(
+            child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 30.0),
+          child: Column(
+            children: [
+              const Text(
+                "Create your account",
+                style: TextStyle(
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.w700,
                 ),
               ),
-            ),
-            const SizedBox(height: 25.0),
-            TextField(
-              controller: _phoneNumber,
-              obscureText: false,
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                labelText: "Phone Number",
-                hintText: "Example: 81800000000",
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10.0),
+              const Divider(height: 30.0, color: Colors.grey),
+              const SizedBox(height: 20),
+              TextField(
+                controller: _nameController,
+                decoration: InputDecoration(
+                  labelText: "Full legal first and middle name(s)",
+                  hintText: "Full legal first and middle name(s)",
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0)),
                 ),
               ),
-            ),
-            const SizedBox(height: 25.0),
-            TextField(
-              controller: _email,
-              obscureText: false,
-              decoration: InputDecoration(
-                hintText: "Email Address",
-                labelText: "Email Address",
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10.0),
+              const SizedBox(height: 25.0),
+              TextField(
+                controller: _legalSurname,
+                obscureText: false,
+                decoration: InputDecoration(
+                  labelText: "Full legal Surname",
+                  hintText: "Full legal Surname",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
                 ),
               ),
-            ),
-            const SizedBox(height: 25.0),
-            TextField(
-              controller: _passWord,
-              obscureText: true,
-              decoration: InputDecoration(
-                hintText: "Password",
-                labelText: "Password",
-                
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10.0),
+              const SizedBox(height: 25.0),
+              TextField(
+                controller: _phoneNumber,
+                obscureText: false,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: "Phone Number",
+                  hintText: "Example: 81800000000",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
                 ),
               ),
-            ),
-            const SizedBox(height: 10),
-            Text(
-              "I confirm the information provided is correct as they appear on my legal document",
-              style: TextStyle(
-                color: Colors.red,
-                fontWeight: FontWeight.w400,
-                fontSize: 16,
+              const SizedBox(height: 25.0),
+              TextField(
+                controller: _email,
+                obscureText: false,
+                decoration: InputDecoration(
+                  hintText: "Email Address",
+                  labelText: "Email Address",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                ),
               ),
+              const SizedBox(height: 25.0),
+              TextField(
+                controller: _passWord,
+                obscureText: true,
+                decoration: InputDecoration(
+                  hintText: "Password",
+                  labelText: "Password",
+                  
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10),
+              Text(
+                "I confirm the information provided is correct as they appear on my legal document",
+                style: TextStyle(
+                  color: Colors.red,
+                  fontWeight: FontWeight.w400,
+                  fontSize: 16,
+                ),
+              ),
+              const SizedBox(
+                height: 25.0,
+              ),
+             Center(
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color(0xFFDCDCDC),
+                        minimumSize: const Size(317, 56),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10)),
+                      ),
+                      onPressed: () {},
+                      child: const Text(
+                        'Create your account',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                          fontFamily: 'Inter',
+                          fontWeight: FontWeight.w600,
+                          height: 1.50,
+                        ),
+                      ),
+             
+           ), ),
+           const SizedBox(
+              height: 32, ),
+    
+              const Text.rich(
+              TextSpan(
+                children: [
+                  TextSpan(
+                    text: 'Got an account? ',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 16,
+                      fontFamily: 'Inter',
+                      fontWeight: FontWeight.w300,
+                      height: 1.50,
+                    ),
+                  ),
+                  TextSpan(
+                    text: 'Sign in',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 16,
+                      fontFamily: 'Inter',
+                      fontWeight: FontWeight.w600,
+                      height: 1.50,
+                    ),
+                  ),
+                ],
+              ),
+              textAlign: TextAlign.center,
             ),
             const SizedBox(
-              height: 25.0,
-            ),
-           Center(
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: const Color(0xFFDCDCDC),
-                      minimumSize: const Size(317, 56),
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10)),
-                    ),
-                    onPressed: () {},
-                    child: const Text(
-                      'Create your account',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 16,
-                        fontFamily: 'Inter',
-                        fontWeight: FontWeight.w600,
-                        height: 1.50,
-                      ),
-                    ),
+              height: 88,
+            )
+           ],
+    
            
-         ), ),
-         const SizedBox(
-            height: 32, ),
-
-            const Text.rich(
-            TextSpan(
-              children: [
-                TextSpan(
-                  text: 'Got an account? ',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 16,
-                    fontFamily: 'Inter',
-                    fontWeight: FontWeight.w300,
-                    height: 1.50,
-                  ),
-                ),
-                TextSpan(
-                  text: 'Sign in',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 16,
-                    fontFamily: 'Inter',
-                    fontWeight: FontWeight.w600,
-                    height: 1.50,
-                  ),
-                ),
-              ],
-            ),
-            textAlign: TextAlign.center,
           ),
-          const SizedBox(
-            height: 88,
-          )
-         ],
-
-         
-        ),
-
+    
+          
+        ),),
+    
+    
         
-      ),),
-
-
-      
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        backgroundColor: Colors.white,
-        child: const Icon(Icons.message_outlined),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          backgroundColor: Colors.white,
+          child: const Icon(Icons.message_outlined),
+        ),
+        
       ),
-      
     );
   }
 }
