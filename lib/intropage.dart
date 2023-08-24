2c90cb9578bc6110483de00aef300312bd916130
Makanakyjr <olayinkakenahmed@gmail.com>
2023-08-24 12:54:30 +0100

    added validator mode

--- lib/intropage.dart
+++ lib/intropage.dart
@@ -23,162 +23,202 @@ class _IntroPageState extends State<IntroPage> {
       body: SingleChildScrollView(
         child: SafeArea(
           child: Padding(
-            key: _formKey,
             padding:
                 const EdgeInsets.symmetric(horizontal: 20.0, vertical: 30.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text(
-                  "Create your account",
-                  style: TextStyle(
-                    fontSize: 24.0,
-                    fontWeight: FontWeight.w700,
-                  ),
-                ),
-                const Divider(height: 30.0, color: Colors.grey),
-                const SizedBox(height: 20),
-                TextField(
-                  controller: _nameController,
-                  decoration: InputDecoration(
-                    labelText: "Full legal first and middle name(s)",
-                    hintText: "Full legal first and middle name(s)",
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10.0)),
-                  ),
-                ),
-                const SizedBox(height: 25.0),
-                TextField(
-                  controller: _legalSurname,
-                  obscureText: false,
-                  decoration: InputDecoration(
-                    labelText: "Full legal Surname",
-                    hintText: "Full legal Surname",
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10.0),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    "Create your account",
+                    style: TextStyle(
+                      fontSize: 24.0,
+                      fontWeight: FontWeight.w700,
                     ),
                   ),
-                ),
-                const SizedBox(height: 25.0),
-                TextField(
-                  controller: _legalSurname,
-                  obscureText: false,
-                  decoration: InputDecoration(
-                    labelText: "Full legal Surname",
-                    hintText: "Full legal Surname",
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 25.0),
-                PhoneNumberInput(
-                    initialCountry: 'NG',
-                    locale: 'en',
-                    countryListMode: CountryListMode.dialog,
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10.0),
-                    )),
-                const SizedBox(height: 25.0),
-                TextField(
-                  controller: _email,
-                  obscureText: false,
-                  decoration: InputDecoration(
-                    hintText: "Email Address",
-                    labelText: "Email Address",
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 25.0),
-                TextField(
-                  controller: _passWord,
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    hintText: "Password",
-                    labelText: "Password",
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 10),
-                Text(
-                  "I confirm the information provided is correct as they appear on my legal document",
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontWeight: FontWeight.w400,
-                    fontSize: 16,
-                  ),
-                ),
-                const SizedBox(
-                  height: 25.0,
-                ),
-                Center(
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: const Color(0xFFDCDCDC),
-                      minimumSize: const Size(317, 56),
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10)),
-                    ),
-                    onPressed: () {
-                      print(_nameController.text);
-                      print(_email.text);
-                      print(_passWord.text);
-                      if (_formKey.currentState!.validate()) {
-                        ScaffoldMessenger.of(context).showSnackBar(
-                            const SnackBar(
-                                content: Text("User validation successful")));
+                  const Divider(height: 30.0, color: Colors.grey),
+                  const SizedBox(height: 20),
+                  TextFormField(
+                    controller: _nameController,
+                    
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "This field cannot be empty";
+                      } else {
+                        return null;
                       }
                     },
-                    child: const Text(
-                      'Create your account',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 16,
-                        fontFamily: 'Inter',
-                        fontWeight: FontWeight.w600,
-                        height: 1.50,
+                    autovalidateMode: AutovalidateMode.onUserInteraction,
+                    decoration: InputDecoration(
+                      labelText: "Full legal first and middle name(s)",
+                      hintText: "Full legal first and middle name(s)",
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0)),
+                    ),
+                  ),
+                  const SizedBox(height: 25.0),
+                  TextFormField(
+                    controller: _legalSurname,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "This field cannot be empty";
+                      } else {
+                        return null;
+                      }
+                    },
+                    obscureText: false,
+                    decoration: InputDecoration(
+                      labelText: "Full legal Surname",
+                      hintText: "Full legal Surname",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
                       ),
                     ),
                   ),
-                ),
-                const SizedBox(
-                  height: 32,
-                ),
-                const Text.rich(
-                  TextSpan(
-                    children: [
-                      TextSpan(
-                        text: 'Got an account? ',
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 16,
-                          fontFamily: 'Inter',
-                          fontWeight: FontWeight.w300,
-                          height: 1.50,
-                        ),
+                  const SizedBox(height: 25.0),
+                  TextFormField(
+                    controller: _legalSurname,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "This field cannot be empty";
+                      } else {
+                        return null;
+                      }
+                    },
+                    obscureText: false,
+                    decoration: InputDecoration(
+                      labelText: "Nationality",
+                      hintText: "Country of Birth",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
                       ),
-                      TextSpan(
-                        text: 'Sign in',
+                    ),
+                  ),
+                  const SizedBox(height: 25.0),
+                  PhoneNumberInput(
+                      initialCountry: 'NG',
+                      locale: 'en',
+                      countryListMode: CountryListMode.dialog,
+                      
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      )),
+                  const SizedBox(height: 25.0),
+                  TextFormField(
+                    controller: _email,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "This field cannot be empty";
+                      } else {
+                        return null;
+                      }
+                    },
+                    obscureText: false,
+                    decoration: InputDecoration(
+                      hintText: "Email Address",
+                      labelText: "Email Address",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 25.0),
+                  TextFormField(
+                    controller: _passWord,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "This field cannot be empty";
+                      } else {
+                        return null;
+                      }
+                    },
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: "Password",
+                      labelText: "Password",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  Text(
+                    "I confirm the information provided is correct as they appear on my legal document",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.w400,
+                      fontSize: 16,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 25.0,
+                  ),
+                  Center(
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color(0xFFDCDCDC),
+                        minimumSize: const Size(317, 56),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10)),
+                      ),
+                      onPressed: () {
+                        print(_nameController.text);
+                        print(_email.text);
+                        print(_passWord.text);
+                        if (_formKey.currentState!.validate()) {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(
+                                  content: Text("User validation successful")));
+                        }
+                      },
+                      child: const Text(
+                        'Create your account',
                         style: TextStyle(
-                          color: Colors.black,
+                          color: Colors.white,
                           fontSize: 16,
                           fontFamily: 'Inter',
                           fontWeight: FontWeight.w600,
                           height: 1.50,
                         ),
                       ),
-                    ],
+                    ),
                   ),
-                  textAlign: TextAlign.center,
-                ),
-                const SizedBox(
-                  height: 88,
-                )
-              ],
+                  const SizedBox(
+                    height: 32,
+                  ),
+                  const Text.rich(
+                    TextSpan(
+                      children: [
+                        TextSpan(
+                          text: 'Got an account? ',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 16,
+                            fontFamily: 'Inter',
+                            fontWeight: FontWeight.w300,
+                            height: 1.50,
+                          ),
+                        ),
+                        TextSpan(
+                          text: 'Sign in',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 16,
+                            fontFamily: 'Inter',
+                            fontWeight: FontWeight.w600,
+                            height: 1.50,
+                          ),
+                        ),
+                      ],
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(
+                    height: 88,
+                  )
+                ],
+              ),
             ),
           ),
         ),
